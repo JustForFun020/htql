17ef3b018c95fccafcb1c0ffa75fc2bac7977ccc
Truong Hoang <hoangtrungtruongyt2020@gmail.com>
2024-05-13 22:40:41 +0700

    nextui: home page ui

---
+++ next-ui/__mock__/user.ts
@@ -0,0 +1,10 @@
+export const fakeUser = {
+    email: 'davidSmith@bookshop.com',
+    fullName: 'David Smith',
+    address: '1234 Main St, New York, NY 10001',
+    dateOfBirth: '01/01/1990',
+    phoneNumber: '123-456-7890',
+    username: 'davidSmith',
+    role: 'Manager',
+    avatar: ''
+}
--- next-ui/src/components/Home.tsx
+++ next-ui/src/components/Home.tsx
@@ -2,19 +2,48 @@
 
 import { UserContext } from '@/context/_userContext';
 import { AppDispatch, RootState } from '@/redux/store';
+import { Avatar, Col, List, Row } from 'antd';
 import React, { useContext, useEffect } from 'react';
 import { ConnectedProps, connect } from 'react-redux';
+import { fakeUser } from '../../__mock__/user';
+import { UserOutlined } from '@ant-design/icons';
+import style from '@/styles/main.module.scss';
+import clsx from 'clsx';
 
 interface IHomeProps extends PropsFromRedux {}
 
 function Home(props: IHomeProps) {
   const { user } = props;
-  
+
   return (
-    <div>
-      <h1>Welcome to Home</h1>
-      <h2>{user.username}</h2>
-    </div>
+    <Row className={clsx(style.home__provider)}>
+      <Col span={8} className={clsx(style.primary)}>
+        <Avatar icon={<UserOutlined />} size={64} />
+        <div className={clsx(style.username)}>{fakeUser.username}</div>
+      </Col>
+      <Col span={16} className={clsx(style.second)}>
+        <List>
+          <List.Item>
+            <h3>Full Name:</h3> <span>{fakeUser.fullName} </span>
+          </List.Item>
+          <List.Item>
+            <h3>Date of Bird:</h3> <span>{fakeUser.dateOfBirth}</span>
+          </List.Item>
+          <List.Item>
+            <h3>Email:</h3> <span>{fakeUser.email}</span>
+          </List.Item>
+          <List.Item>
+            <h3>Address:</h3> <span>{fakeUser.address}</span>
+          </List.Item>
+          <List.Item>
+            <h3>Phone Number:</h3> <span>{fakeUser.phoneNumber}</span>
+          </List.Item>
+          <List.Item>
+            <h3>Position:</h3> <span>{fakeUser.role}</span>
+          </List.Item>
+        </List>
+      </Col>
+    </Row>
   );
 }
 
